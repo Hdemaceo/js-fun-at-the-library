96e1ffa7791196b3b30adef78ca901ac84e7032f
Demaceo Howard <hdemaceo@gmail.com>
2020-08-22 17:53:13 -0600

    Complete shelf.js and shelf-test.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,8 +1,45 @@
 
 
+function shelfBook(book, shelf){
+  if(shelf.length < 3){
+    return shelf.unshift(book)
+  }
+}
+
+function unshelfBook(name, shelf){
+  for(var i = 0; i < shelf.length; i++){
+    if(shelf[i].title === name){
+      shelf.splice(i, 1);
+    }
+  }
+  return shelf
+}
+
+function listTitles(shelf){
+  var bookTitles = "";
+  for(var i = 0; i < shelf.length; i++){
+    if(i !== shelf.length-1){
+        bookTitles += shelf[i].title + ", ";
+    } else {
+      bookTitles += shelf[i].title
+    }
+  }
+  return bookTitles;
+}
+
+function searchShelf(shelf, name) {
+  for (var i = 0; i < shelf.length; i++) {
+    if(shelf[i].title === name){
+      return true;
+    }
+  }
+  return false;
+}
+
+
 module.exports = {
-  // shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
-  // listTitles: listTitles,
-  // searchShelf: searchShelf
+  shelfBook: shelfBook,
+  unshelfBook: unshelfBook,
+  listTitles: listTitles,
+  searchShelf: searchShelf
 };
